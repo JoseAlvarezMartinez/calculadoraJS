dae17ba820ff254339f2fb5c9c7497d01774fca2
José Alvarez Martinez <97672139+JoseAlvarezMartinez@users.noreply.github.com>
2026-04-01 09:42:04 -0300

    TAREA

--- js/tarjeta.js
+++ js/tarjeta.js
@@ -1,7 +1,14 @@
 const cardNombre = document.querySelector("#card-nombre")
 const nombreInput = document.querySelector("#nombre")
-
+const cardNumero = document.querySelector("#card-numero");
+const numeroInput = document.querySelector("#numero")
 nombreInput.addEventListener("input", () => {
-    cardNombre.textContent = nombreInput.value
+    cardNombre.textContent = nombreInput.value || "Pepito Fulano";
+})
+
+numeroInput.addEventListener("input", () => {
+    cardNumero.textContent = numeroInput.value || "0000 0000 0000 0000";
+
+    console.log(cardNumero.textContent);
     
 })

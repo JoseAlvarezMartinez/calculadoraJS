1fe05df702b6078e035962efe4c244fb2038e557
José Alvarez Martinez <97672139+JoseAlvarezMartinez@users.noreply.github.com>
2026-04-01 08:56:26 -0300

    archivo js

---
+++ js/calculadora.js
@@ -0,0 +1,32 @@
+// Recopilar elementos del DOM
+const display = document.querySelector("#display")
+const botones = document.querySelectorAll("button")
+
+let currentInput = "";
+
+botones.forEach(btn => btn.addEventListener("click", () => {
+   const valor = btn.textContent
+   handleInput(valor)
+}))
+
+function handleInput(valor){
+    if(valor === "RESET"){
+        currentInput = "";
+        display.textContent = "0"
+    }
+    if(valor === "DEL"){
+        currentInput = currentInput.slice(0,-1)
+        display.textContent = currentInput || 0
+    }
+    if(valor === "="){
+        try{
+            currentInput = eval(currentInput).toString()
+            display.textContent = currentInput;
+        }catch{
+            display.textContent = "Error en la operación"
+        }
+    }
+    
+    currentInput += valor;
+    display.textContent = currentInput;
+}
